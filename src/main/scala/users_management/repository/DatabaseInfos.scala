9064fa855a91bf59ca51428bf96da94c5d9837cb
1ngVG <giannini.v@outlook.com>
2025-06-07 10:58:37 +0200

    docs: Scala doc for traits and case classes

--- src/main/scala/users_management/repository/DatabaseInfos.scala
+++ src/main/scala/users_management/repository/DatabaseInfos.scala
@@ -1,5 +1,14 @@
 package users_management.repository
 
+/** Case class representing MongoDB database connection and collection information.
+ *
+ * This class encapsulates the necessary information to connect to and use a MongoDB database,
+ * including the collection name, connection URI and database name.
+ *
+ * @param collectionName The name of the MongoDB collection to use
+ * @param mongoUri       The MongoDB connection URI string (e.g. mongodb://host:port)
+ * @param databaseName   The name of the MongoDB database to use
+ */
 case class DatabaseInfos(
                           collectionName: String,
                           mongoUri: String,

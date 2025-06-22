28feb1380b535255864638756cb6ad873b94a151
1ngVG <giannini.v@outlook.com>
2025-06-22 12:22:52 +0200

    tests: PreCommitTest

--- src/test/scala/preCommitTest/PreCommitTest.scala
+++ src/test/scala/preCommitTest/PreCommitTest.scala
@@ -6,18 +6,18 @@ class PreCommitTest extends AnyFunSuite:
 
   // Codice volutamente malformattato per testare scalafmt
   val nonFormattedCode =
-
-
     "class BadFormatting{def method(   x:Int,y:     Int)=x+y}"
 
   // Codice che dovrebbe generare warning di scalafix
   @deprecated var unusedVariable = 42 // warning: var potrebbe essere val
 
+
   test("verify code formatting") {
-
     // Il codice seguente è malformattato e dovrebbe essere corretto da scalafmt
-    def badlyFormattedMethod(a: Int, b: Int) =
 
+
+
+    def badlyFormattedMethod(a: Int, b: Int) =
       a + b
 
     // Questo test passerà sempre, serve solo per vedere se scalafmt lo formatta
